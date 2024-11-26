f1a4ba506575aef9046f3be383e0005c00b8ccd8
Tiago Rezende <tiago.rezende@unity3d.com>
2024-11-26 19:12:06 +0100

    Test development branch

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vercel Test</h1>
+      <h1>Vercel Test - Development Version</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
